848d2b92ab6afeedc6924fe41a34028761b05ec9
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-16 11:55:34 +0530

    difference between onload and pageLoadedFunction in JS

--- project-04-jQuery-ready/index.js
+++ project-04-jQuery-ready/index.js
@@ -6,13 +6,18 @@
 // instead of document. ready. We can use an anonymous function and 
 // make sure that document is ready to use. jQuery relies heavily on the document object. 
 // That is, why we have to make sure that it is loaded. 
-/*
+
 $(function(){
     console.log("Document is ready to use.");
 })
-*/
-document.addEventListener("DOMContentLoaded", pageLoadedFunction());
 
-function pageLoadedFunction(){
-    console.log("Document is ready to use.");
+
+
+document.addEventListener("DOMContentLoaded", pageLoadedFunction('1'));
+
+function pageLoadedFunction(num){
+    console.log("DOM is ready to use."+" ("+num+")");
 }
+
+
+document.onload = pageLoadedFunction("2");
