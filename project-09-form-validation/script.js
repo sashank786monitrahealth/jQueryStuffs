29c662e13dfc7b6753cd9f43174080c4dce3b8d8
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-27 22:38:23 +0530

    logging values from input elements to the console

--- project-09-form-validation/script.js
+++ project-09-form-validation/script.js
@@ -6,7 +6,14 @@ $(document).ready(function(){
 
     function validateMe(){
       console.log("validate me");
-      //$('#myForm').submit();
+     
+      var okay = false;
+      //console.log($('#myForm input'));
+      // grabs each element and allows us to loop through it
+      $('#myForm input').each(function(){
+          console.log($(this).val())
+      })
+      if (okay){ $('#myForm').submit(); }
     };
 
 })
