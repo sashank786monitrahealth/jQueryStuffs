d5bf5c54d54528a65823edda3fdba054e8b04b3a
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-28 09:40:29 +0530

    added regular expression (/^[^\s@]+@[^\s@]+\.[^\s@]+$/) to validate email

--- project-09-form-validation/script.js
+++ project-09-form-validation/script.js
@@ -6,15 +6,21 @@ $(document).ready(function(){
 
     function validateMe(){
       console.log("validate me");
-     
+      var emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       var error = [];
       //console.log($('#myForm input'));
       // grabs each element and allows us to loop through it
       $('#myForm input').each(function(){
           if ($(this).val().length > 4){
-            console.log($(this).attr("name"))
+            console.log($(this).attr("name"));
+            if(!emailReg.test($(this).val())){
+                console.log("invalid email");
+            } else {
+                console.log("valid email");
+            }
         } else {
             error.push($(this).attr("name"));
+
         }
       })
       if (error.length == 0){ $('#myForm').submit(); }
