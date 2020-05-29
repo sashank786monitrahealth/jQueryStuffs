07993c234e0ba8b701bf111951c584b6e6df8d6a
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-29 08:38:34 +0530

    added Start Game button

--- project-10-color-guess-game/script.js
+++ project-10-color-guess-game/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     $('#output').hide();
+    $('#start').click(startGame);
+    $('#start').css({
+        border:'1px solid black',
+        textAlign:'center',
+        padding:'10px',
+        width:'150px',
+        backgroundColor:'red',
+        fontSize:'2em',
+        marginBottom:'10px'
+    })
+
+    function startGame(){
+        console.log("game started");
+    }
+
 })
