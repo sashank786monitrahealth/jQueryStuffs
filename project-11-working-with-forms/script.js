7815faa7fe345f78c94ad6f3b3a76c3ea675896c
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-31 11:07:57 +0530

    checked the checkboxes using attr method

--- project-11-working-with-forms/script.js
+++ project-11-working-with-forms/script.js
@@ -6,4 +6,12 @@ $('input[type="date"]').css("background-color","red");
 
 $('input[type="color"]').css("background-color","skyblue");
 
+$('.btn').click(function(){
+    var myCheckBoxes = $('input[type="checkbox"]').attr({
+        checked:true
+    });
+    console.log(myCheckBoxes.val());
+})
+
+
 })
