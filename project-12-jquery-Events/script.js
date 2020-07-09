6dd969ed8efa64b540ae6b7cbde08a0a309df350
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-09 16:55:31 +0530

    add hover listener

--- project-12-jquery-Events/script.js
+++ project-12-jquery-Events/script.js
@@ -1,7 +1,14 @@
 $(function(){
     var counter = 0;
+    $('.highlight').dblclick(function(){
+        $(this).text('Double Click!!')
+    })
+
     $('.highlight').click(function(){
         $(this).text('clicked '+counter);
         counter++;
+    })
+    $('.highlight').hover(function(){
+        $(this).text('Hover over Me!!');
     })
 })
