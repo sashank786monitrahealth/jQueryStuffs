4b25871fe4dc261624c9b364c86f776c2e0ef144
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-10 07:08:58 +0530

    on blur

--- project-13-jquery-Events-Keyboard/script.js
+++ project-13-jquery-Events-Keyboard/script.js
@@ -3,4 +3,8 @@ $(function(){
     $('input').focus(function(){
         $(this).css('background-color','yellow');
     })
+
+    $('input').blur(function(){
+        $(this).css('background-color','white');
+    })
 })
