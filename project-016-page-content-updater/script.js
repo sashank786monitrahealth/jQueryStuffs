529d8b058d550871082ae714cb99efb324c15bd8
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-17 00:32:23 +0530

    before

--- project-016-page-content-updater/script.js
+++ project-016-page-content-updater/script.js
@@ -3,6 +3,7 @@ $(
         var counter = 0;
         $('.highlight').click(function(){
             counter++;
+            $(this).before('BEFORE '+counter);
             $(this).append('clicked '+counter);
             $(this).prepend('Pre '+counter);
         })
