599cc286ac9dc24e2c7e0d22e23712601fe7568f
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-16 17:33:30 +0530

    add jQuery styles

--- project-15-Page-Manipulation/script.js
+++ project-15-Page-Manipulation/script.js
@@ -1,5 +1,10 @@
 $(
     function(){
-        
+        $('.highlight').css({
+            backgroundColor:'red',
+            padding:'10px',
+            margin:'5px',
+            border:'1px solid #ddd'
+        })
     }
 )
