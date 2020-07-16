93bbca2d8404e072b8c344da8d50524f604226a7
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-16 17:41:37 +0530

    add transition to hiding element

--- project-15-Page-Manipulation/script.js
+++ project-15-Page-Manipulation/script.js
@@ -6,5 +6,11 @@ $(
             margin:'5px',
             border:'1px solid #ddd'
         })
-    }
-)
+
+        $('.highlight').click(function(){
+            $(this).hide(5000,function(){
+                $(this).next().css('background-color','blue')
+            })
+        })
+
+})
