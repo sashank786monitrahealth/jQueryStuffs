efc1dfe761ab6ca54bd46d31317f2acfb103f71f
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-19 18:59:22 +0530

    on click event listener

---
+++ project-018-jquery-forms/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    //$('input[type="button"]').on('click', submitForm);
+    //$('button').click(submitForm);
+
+    $('button').on('click', submitForm);
+
+    function submitForm(){
+        console.log('submit clicked');
+    }
+
+    // submitButton is the function we will call on clicking the button
+
+
+ 
+})
