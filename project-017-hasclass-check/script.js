e146780b111123afa30768443427eaa0721347bc
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-19 12:37:16 +0530

    checkClass - change background color

--- project-017-hasclass-check/script.js
+++ project-017-hasclass-check/script.js
@@ -4,8 +4,17 @@ $(
         $('div > div > div').click(function(){
             //console.log($(this).html());
             // get the first child of the mainDiv
+
+            var backClr = checkClass ? 'green' : 'red';
+
+            $('h1').css('background-color',backClr);
+        })
+
+        function checkClass(whatClass){
+
             var classChecker = $('#mainDiv').children().first().hasClass('highlight')?'Yes':'No';
             console.log(classChecker);
-        })
+            return classChecker;
+        }
     }
 )
