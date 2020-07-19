5d811f7598370b57c62bdbdf14de7a3e50384013
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-19 08:24:15 +0530

    log this

--- project-017-hasclass-check/script.js
+++ project-017-hasclass-check/script.js
@@ -2,7 +2,7 @@ $(
     function(){
         // select div inside div
         $('div > div > div').click(function(){
-            console.log($(this).text);
+            console.log($(this));
         })
     }
 )
