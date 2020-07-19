7eac81dc5f34a2b7cd4b65bd25ee76c3eacddad9
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-19 11:33:36 +0530

    get the first child of the mainDiv element

--- project-017-hasclass-check/script.js
+++ project-017-hasclass-check/script.js
@@ -3,6 +3,8 @@ $(
         // select div inside div
         $('div > div > div').click(function(){
             console.log($(this).html());
+            // get the first child of the mainDiv
+            console.log($('#mainDiv').first());
         })
     }
 )
