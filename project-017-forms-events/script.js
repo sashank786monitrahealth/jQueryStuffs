ef68ab1a6b43aa911d6def95450e71ce436e5e83
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-25 02:39:36 +0530

    init

--- project-017-forms-events/script.js
+++ project-017-forms-events/script.js
@@ -1,3 +1,26 @@
 $(function(){
-    
+    $('button').on('click',submitForm);
+
+    function submitForm(){
+        var formValues = $('form').serialize();
+        var tempHolder = formValues.split('&');
+        outputUpdate(tempHolder);
+    }
+
+    function outputUpdate(){
+        $('#mainDiv').empty();
+
+        $('input').css('background-color','white');
+
+        $.each(tempHolder, function(index,value){
+            var elementName = value.split('=');
+            var $el = $('input[name"'+elementName[0]+'"]');
+
+            if ($el.val() == ''){
+                $el.css('background-color','red')
+            }
+
+            $('#mainDiv').append(elementName[0]+' '+elementName[1]+'<br>');
+        })
+    }
 })
