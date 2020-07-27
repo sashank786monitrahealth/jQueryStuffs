f10100ba7a985f2b4abe91088a76e9684b8483bf
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-27 11:39:10 +0530

    on change of color change image color

--- project-019-jquery-placeholder/script.js
+++ project-019-jquery-placeholder/script.js
@@ -5,14 +5,25 @@ $(function(){
   $('input[type="color"]').first().before('Background Color :');
   $('input[type="color"]').last().before('Text Color :');
 
+
+  $('input[type="color"]').change(function(){
+    event.preventDefault();
+    var clrValues = $('input[type="color"]');
+    var colorBack = cleanHash(clrValues.first().val());
+    var colorFront = cleanHash(clrValues.last().val());
+    console.log("colorBack = "+colorBack);
+    console.log("colorFront = "+colorFront);
+    var imagePath = 'http://via.placeholder.com/350x150/'+colorBack+'/'+colorFront;
+    $('img').attr('src',imagePath);
+});
+
   $('input[type="submit"]').click(function(){
       event.preventDefault();
-      var clrValues = $('input[type="color"]');
-      var colorBack = clrValues.first().val();
-      var colorFront = clrValues.last().val();
-      console.log("colorBack = "+colorBack);
-      console.log("colorFront = "+colorFront);
-      var imagePath = 'http://via.placeholder.com/350x150';
-      $('img').attr('src',imagePath);
   })
+
+
+function cleanHash(arg){
+    return arg.replace('#','');
+}
+
 })
