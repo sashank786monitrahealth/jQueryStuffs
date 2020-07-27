615d9b1854600b75e0a7d3ab8538cc6cff832577
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-27 10:33:51 +0530

    add colons after text

--- project-019-jquery-placeholder/script.js
+++ project-019-jquery-placeholder/script.js
@@ -1,5 +1,8 @@
 $(function(){
 
-  $('input').first().before('Image Text ');
-
+  $('input').first().before('Image Text :');
+  $('input').after('<br>');
+  $('input[type="color"]').first().before('Background Color :');
+  $('input[type="color"]').last().before('Text Color :');
+  $('input').f
 })
