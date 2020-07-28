71f6de9923a1f35991dcba3e5f083b8886d06689
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-28 21:32:36 +0530

    include also the password field

--- project-021-form-error-classes/script.js
+++ project-021-form-error-classes/script.js
@@ -2,7 +2,7 @@ $(function(){
     $('button').on('click',submitForm);
 
     function submitForm(){
-        var $el = $('input[type="text"]');
+        var $el = $('input[type="text"],input[type="password"]');
         console.log($el);
     }
 
