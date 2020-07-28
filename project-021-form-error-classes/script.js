ad92b207621ef27aee6d0c0400995e38c64c7217
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-28 09:10:37 +0530

    update script.js

--- project-021-form-error-classes/script.js
+++ project-021-form-error-classes/script.js
@@ -2,42 +2,11 @@ $(function(){
     $('button').on('click',submitForm);
 
     function submitForm(){
-        var formValues = $('form').serialize();
-        var tempHolder = formValues.split('&');
-        var $a = $('input[name="agree1"]');
-        if ($a.prop('checked')){
-            outputUpdate(tempHolder);
-        } else {
-            alert('Please agree to the Terms of Service before you submit.');
-        }
-
+        
     }
 
-$('input[name="agree2"]').change(function(){
-    //alert('Checked Button '+$(this).attr('name'))
-    $('input').removeAttr('placeholder');
-})
+
 
         
 
-
-    function outputUpdate(tempHolder){
-        $('#mainDiv').empty();
-
-        $('input').css('background-color','white');
-
-        $.each(tempHolder, function(index,value){
-            var elementName = value.split('=');
-            var $el = $('input[name="'+elementName[0]+'"]');
-
-            if ($el.val() == ''){
-                $el.css('background-color','red')
-                $el.after('<div class="error">'+elementName[0]+' value is missing'+'<div>')
-            } else {
-                $el.nextAll('.error').remove();
-            }
-
-            $('#mainDiv').append(elementName[0]+' '+elementName[1]+'<br>');
-        })
-    }
 })
