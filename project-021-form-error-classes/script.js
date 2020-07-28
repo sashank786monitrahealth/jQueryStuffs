650534af68260c1af7621da9e96f87b9ed9d2db2
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-28 21:53:48 +0530

    print only values of individual elements

--- project-021-form-error-classes/script.js
+++ project-021-form-error-classes/script.js
@@ -5,7 +5,7 @@ $(function(){
         var $el = $('input[type="text"],input[type="password"]');
 
         for(x=0;x<$el.length;x++){
-            console.log($el.eq(x));
+            console.log($el.eq(x).val());
         }
 
         //console.log($el);
