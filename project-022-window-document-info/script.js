aaf5db6e29e53b3cca8f8413ea07b233087fba27
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-29 03:14:09 +0530

    get information about the elements

--- project-022-window-document-info/script.js
+++ project-022-window-document-info/script.js
@@ -3,11 +3,15 @@ $(function(){
     $('h1').on('click', function(){
         var docHeight = $(document).width();
         var winHeight = $(window).width();
-        console.log($(window));
-        console.log($(document));
+        //console.log($(window));
+        //console.log($(document));
 
-        console.log(docHeight)
-        console.log(winHeight)
+        //console.log(docHeight)
+        //console.log(winHeight);
+
+        var $el = $('#wrapper > div').last();
+        console.log($el);
+        console.log($el.height())
     });
         
 
