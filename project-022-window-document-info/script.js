46188f701afcdef2c383b67d8bec815128e15910
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-29 03:00:58 +0530

    log - document height - window height

--- project-022-window-document-info/script.js
+++ project-022-window-document-info/script.js
@@ -1,5 +1,14 @@
 $(function(){
+   
+    $('h1').on('click', function(){
+        var docHeight = $(document).height();
+        var winHeight = $(window).height();
+        console.log($(window));
+        console.log($(document));
 
+        console.log(docHeight)
+        console.log(winHeight)
+    });
         
 
 })
