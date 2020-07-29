4edbf1729f7f819debb7acd09f1a1a7a6a112452
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-29 15:35:39 +0530

    based on mouse-events take actions

--- project-024-toggle-images/script.js
+++ project-024-toggle-images/script.js
@@ -1,9 +1,11 @@
 $(function(){
 
- $('img').css({width:'50px',height:'50px'});
- 
- $('.highlight').mouseleave(function(){
-     $(this).height('200px')
- })
+    $('img').css({width:'50px',height:'50px'});
+
+    $('.highlight').mouseleave(function(){
+       $(this).height('80px');
+    }).mouseenter(function(){
+        $(this).height('40px');
+    })
 
 })
