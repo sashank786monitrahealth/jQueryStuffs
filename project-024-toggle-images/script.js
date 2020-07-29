a2036c107da20f56742891452e9877f9aa6bca9d
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-29 13:36:39 +0530

    on mouseleave

--- project-024-toggle-images/script.js
+++ project-024-toggle-images/script.js
@@ -1,15 +1,9 @@
 $(function(){
 
- $('img').css({width:'50px',height:'50px'})
- $('img').click(function(){
-     var txtInfo = $('input[type="text"]').val()
-     var tempSrc = $(this).attr('src');
-     console.log(tempSrc);
-     window.open(tempSrc+'?text='+txtInfo+'','Full Image','Window popup')
- }).mouseenter(function(){
-     $(this).css('border','1px solid red')
- }).mouseout(function(){
-    $(this).css('border','1px solid green')
-})
+ $('img').css({width:'50px',height:'50px'});
+ 
+ $('.highlight').mouseleave(function(){
+     $(this).height('200px')
+ })
 
 })
