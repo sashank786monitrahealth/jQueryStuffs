58ad5501a0ae8cf1f2befc519efdb3e708d63512
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-29 10:05:09 +0530

    read text

--- project-023-popup-image/script.js
+++ project-023-popup-image/script.js
@@ -2,9 +2,10 @@ $(function(){
 
  $('img').css({width:'50px',height:'50px'})
  $('img').click(function(){
+     var txtInfo = $('input[type="text"]').val()
      var tempSrc = $(this).attr('src');
      console.log(tempSrc);
-     window.open(tempSrc,'Full Image','Window popup')
+     window.open(tempSrc+'?text='+txtInfo+'','Full Image','Window popup')
  })
 
 })
