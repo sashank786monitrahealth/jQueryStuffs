4a2ddc4437bfcb95ef313a294a3d55a7437a350d
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-29 17:32:11 +0530

    log the element being clicked

--- project-025-slide-animation/script.js
+++ project-025-slide-animation/script.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.btn').css({
+                    'border':'1px solid black',
+                    'padding':'10px',
+                    'width':'150px'
+                  }).click(function(){
+              console.log( $(this));
+    })
 })
