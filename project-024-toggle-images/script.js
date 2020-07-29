20229a9fc15269919085af4a27e39e22f0781ffe
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-29 15:52:33 +0530

    change color only

--- project-024-toggle-images/script.js
+++ project-024-toggle-images/script.js
@@ -3,10 +3,10 @@ $(function(){
     $('img').css({width:'50px',height:'50px'});
 
     $('.highlight').mouseleave(function(){
-       $(this).height('80px');
-       $(this).css({"background-color":"yellow"})
+       //$(this).height('80px');
+       $(this).css({"background-color":"white"})
     }).mouseenter(function(){
-        $(this).height('40px');
+        //$(this).height('40px');
         $(this).css({"background-color":"lime"})
     })
 
