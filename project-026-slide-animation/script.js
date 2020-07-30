3f2f1f9586a7d218fb1dfb6963ee62e072cd4257
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-30 08:56:06 +0530

    fade functions

--- project-026-slide-animation/script.js
+++ project-026-slide-animation/script.js
@@ -5,17 +5,21 @@ $(function(){
                     'width':'150px'
                   }).click(function(){
               if( $(this).index() == 1){
-                  $('#output2').slideUp(5000, function(){
-                      console.log("slide up read");
+                  $('#output2').fadeIn(5000, function(){
+                      console.log("fading in");
                   });
               };
 
               if($(this).index() == 2){
-                  $('#output2').slideDown(5000);
+                  $('#output2').fadeOut(5000, function(){
+                      console.log("fading out")
+                  });
               }
 
               if($(this).index() == 3){
-                  $('#output3').slideToggle(3000);
+                  $('#output3').fadeToggle(3000,function(){
+                      console.log("fade toggle")
+                  });
               }
     })
 })
