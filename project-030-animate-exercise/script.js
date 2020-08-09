7630d858d92a581659672365edd5798fadf23a28
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-09 06:46:12 +0530

    increase letter spacing using animate method

--- project-030-animate-exercise/script.js
+++ project-030-animate-exercise/script.js
@@ -3,6 +3,13 @@ $(function(){
     $('.btn').click(function(){
        var str = $(this).text().slice(-1);
        console.log(str);
-    })
 
+       if (str=='1'){
+           $('.highlight').animate({
+               opacity:'0.5',
+               letterSpacing:'+=10px'
+           })
+       }
+    })
+    
 })
