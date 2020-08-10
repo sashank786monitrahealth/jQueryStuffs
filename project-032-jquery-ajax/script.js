135fd6f48e92f14982f69e1d42f5cff6a0529874
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-10 07:00:23 +0530

    jQuery hasClass

---
+++ project-032-jquery-ajax/script.js
@@ -0,0 +1,21 @@
+$(function(){
+
+
+    $('.btn').css({
+        'border':'1px solid black'
+        , 'width':'150px'
+        , 'padding':'10px'
+
+    })
+
+    $('.btn').click(function(){
+
+        if($(this).hasClass('btn-1')){
+            console.log('one');
+        }
+
+
+    
+})
+
+})
