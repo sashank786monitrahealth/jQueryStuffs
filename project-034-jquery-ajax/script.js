a85cfa936eb238aa9a7174fe8a4f55228238143e
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-14 07:31:07 +0530

    use getJSON

--- project-034-jquery-ajax/script.js
+++ project-034-jquery-ajax/script.js
@@ -7,10 +7,31 @@ $(function(){
 
     $('.btn-1').click(function(e){
         jQuery.ajax({
-            url:'//raw.githubusercontent.com/sashank786monitrahealth/jsonAJAX/master/db.json'
-        }).done(function(d){
+            url:'https://raw.githubusercontent.com/sashank786monitrahealth/jsonAJAX/master/db.json',
+            dataType: 'json',
+            success: (function(data){
+                     console.log('SUCCESS');
+                     console.log(data);
+            })
+        }).done(function(rp, status, xhr){
             console.log('DONE');
-            console.log(d);
+            console.log(rp);
+            console.log(status);
+
+        }).fail(function(){
+            console.log('FAIL');
+        }).always(function(){
+            console.log('ALWAYS');
         })
     })
+
+
+    $('.btn-2').click(function(e){
+        var url = 'https://raw.githubusercontent.com/sashank786monitrahealth/jsonAJAX/master/db.json';
+        $.getJSON(url, function(data){
+            console.log(data);
+        })
+
+    })
+
 })
