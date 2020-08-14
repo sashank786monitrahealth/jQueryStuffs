ca3ef67520ceeed0cd9c2622808b36ee7ad33b52
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-14 17:27:00 +0530

    connect button 1

--- project-038-jquery-post-data/script.js
+++ project-038-jquery-post-data/script.js
@@ -5,7 +5,7 @@ $(function(){
         , 'padding':'10px'
     })
 
-    $('.btn-3').click(function(e){
+    $('.btn-1').click(function(e){
         $.ajax({
             url:'https://api.github.com/gists'
             , dataType: 'json'
@@ -17,24 +17,6 @@ $(function(){
                     $('#output1').append(data[i].description+'<br><br><br>')
                     })
                 })
-        })
-    })
-
-    $('.btn-2').click(function(e){
-        jQuery.ajax({
-            url:'https://api.randomuser.me/?results=50',
-            dataType: 'json',
-            success: (function(data){
-                     console.log(data);
-                     var html = "<h1>People</h1> <br><br><br>";
-                     $.each(data.results,function(i, n){
-                        
-                        var fullName = n.name.first + ' ' + n.name.last;
-                        var userImage = n.picture.thumbnail;
-                            html += '<span>'+' <img src="'+userImage+'"'+'</span>';
-                        $('#output1').html(html);
-                     })
-            })
         })
     })
 
