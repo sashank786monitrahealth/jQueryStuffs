cccf2b2393933f101843efb9659f145bec6900b1
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-15 08:49:45 +0530

    received api key from google

--- project-040-youtube-api/script.js
+++ project-040-youtube-api/script.js
@@ -2,12 +2,15 @@
 
 https://developers.google.com/youtube/v3/getting-started
 
+Obtianed Key: AIzaSyC1SNw34ZsayRuVLUcp9_6FVFztafmZAHI
+
 */
 //ensuring jQuery is ready
 $(document).ready(function(){
 
 $('#searchButton').click(function(){
     console.log('clicked.');
+    console.log("API key: "+"AIzaSyC1SNw34ZsayRuVLUcp9_6FVFztafmZAHI");
 })
 
 
