cab2545a2a34156bbb5a6b191a45ad6876423c56
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-15 15:47:29 +0530

    show search results

--- project-040-youtube-api/script.js
+++ project-040-youtube-api/script.js
@@ -1,17 +1,28 @@
-/* Obtaining youtube-api
+/* 
 
+Obtaining youtube-api:
+######################
 https://developers.google.com/youtube/v3/getting-started
 
+Youtube-Credentials Page:
+#########################
+https://console.developers.google.com/apis/credentials?project=jquery-youtube-api-286502
 Obtianed Key: AIzaSyC1SNw34ZsayRuVLUcp9_6FVFztafmZAHI
+Complete URL: https://www.googleapis.com/youtube/v3/search?q=test&part=snippet&key=AIzaSyC1SNw34ZsayRuVLUcp9_6FVFztafmZAHI
 
 */
 //ensuring jQuery is ready
 $(document).ready(function(){
 
-$('#searchButton').click(function(){
-    console.log('clicked.');
-    console.log("API key: "+"AIzaSyC1SNw34ZsayRuVLUcp9_6FVFztafmZAHI");
-})
+    $('#searchButton').click(function(){
+        console.log('clicked.');
+        console.log("API key: "+"AIzaSyC1SNw34ZsayRuVLUcp9_6FVFztafmZAHI");
+        var apiKey = "AIzaSyC1SNw34ZsayRuVLUcp9_6FVFztafmZAHI";
+        var url = "https://www.googleapis.com/youtube/v3/search?q=test&part=snippet&key="+apiKey;
+        $.getJSON(url, function(data){
+           console.log(data);
+        })
+    })
 
 
 })
