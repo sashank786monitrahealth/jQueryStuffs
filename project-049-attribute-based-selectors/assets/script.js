a939e0b6d0a5db72299c988a6e5b2235b48a623f
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-19 09:08:50 +0530

    adding class selector

---
+++ project-049-attribute-based-selectors/assets/script.js
@@ -0,0 +1,17 @@
+
+$(function(){
+    $("#btnClick").click(function(){
+        $("[class]").css("color","red");
+    })
+})
+/*
+    $(document).ready(function(){       
+      $("#btnClick").click(function(){            
+          $("[class]").css("color", "red");
+          $("[id]").css("color", "red");
+          $("[id=firstDiv]").css("color", "red");
+          $("[class=paras]").css("color", "red");
+          $("p[class!=paras]").css("color", "red");
+      });         
+   });
+   */
