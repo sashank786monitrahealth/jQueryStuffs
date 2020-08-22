799f7ab39fe28d33fa4cefa6c15c614d6e9c4c51
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-22 08:13:17 +0530

    highlight first elements ^=

---
+++ project-050-attribute-based-selectors-2/assets/script.js
@@ -0,0 +1,22 @@
+
+$(function(){
+    $("#btnClick1").click(function(){
+        $("[id^=first]").css("color","red");
+        //$("[btn]").css("color","red");
+    })
+    $("#btnClick2").click(function(){
+        //$("[class]").css("color","red");
+        $("[id]").css("color","chartreuse");
+    })
+})
+/*
+    $(document).ready(function(){       
+      $("#btnClick").click(function(){            
+          $("[class]").css("color", "red");
+          $("[id]").css("color", "red");
+          $("[id=firstDiv]").css("color", "red");
+          $("[class=paras]").css("color", "red");
+          $("p[class!=paras]").css("color", "red");
+      });         
+   });
+   */
